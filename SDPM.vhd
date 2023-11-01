f31bbcb4f392e25e6d6c8ce9e042135865b9153e
georgia201 <147213175+georgia201@users.noreply.github.com>
2023-11-01 15:46:33 -0400

    Add files via upload

--- SDPM.vhd
+++ SDPM.vhd
@@ -36,7 +36,7 @@ entity SDPM is
     port(START, RESET, CLK: in std_logic;
          OP1, OP2: in std_logic_vector(7 downto 0);
          DONE, OVF, OP_LD, RES_LD, BUFEN1, BUFEN2: out std_logic;
-         SRC2_1, SRC2_0, SRC1_1, SRC1_0: out std_logic;
+         SRC2, SRC1: out std_logic_vector(1 downto 0);
          RES: out std_logic_vector(7 downto 0));
    
 end SDPM;
@@ -49,20 +49,22 @@ component SerialBitAdder
 end component;
 
 component CD74HC299
-    port(A, B, CLK, RESET, MR: in std_logic;
+    port(CLK, MR: in std_logic;
          S: in std_logic_vector(1 downto 0);
          So: out std_logic_vector(1 downto 0);
-         DS0, MRo: out std_logic);  
+         DS0, MRo: out std_logic);
 end component;
 
     type state_type is (idle, S1, S2, S3, S4);
     signal present_state, next_state: state_type:= idle;
+    signal A, B, sum, shift_out, Q7: std_logic;
 
 begin
 
-    adder: SerialBitAdder port map(A => A, B => B, CLK => CLK, RESET => RESET, sum => DS0, CY => open);
+    adder: SerialBitAdder port map(A => A, B => B, CLK => CLK, RESET => RESET, sum => sum, CY => OVF);
+    shift_reg: CD74HC299 port map(CLK => CLK, MR => RESET, S => S, So => SRC2, DS0 => shift_out, MRo => 
 
-    SDPM_process: process(START, RESET, CLK, OP1, OP2, SRC2_1, SRC2_0, SRC1_1, SRC1_0)
+    SDPM_process: process(START, RESET, CLK, OP1, OP2, SRC2, SRC1)
       begin
         case present_state is
             when idle =>
@@ -77,20 +79,23 @@ begin
                 
                 next_state <= S2;
                 
-            when S2 => 
+            when S2 =>                  -- parallel load mode
                 OP_LD <= '1';
-                SRC2_1 <= '1';
-                SRC2_0 <= '1';
-                SRC1_1 <= '1';
-                SRC1_0 <= '1';
+                SRC2 <= "11";
+                SRC1 <= "11";
                 next_state <= S3;
                 
-            when S3 =>
+            when S3 =>                  -- right shift mode
                 OP_LD <= '0';
-                SRC2_1 <= '0';
-                SRC2_0 <= '1';
-                SRC1_1 <= '0';
-                SRC1_0 <= '1';
+                SRC2 <= "01";
+                SRC1 <= "01";
+                
+                temp0 <= 
+                
+                next_state <= S4;
+                
+            when S4 => 
+                
                 
 
 end Behavioral;
